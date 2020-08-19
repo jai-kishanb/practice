73482319800aa570f0cfcc637e4997dc1a9c888b
Jaikishan <Jaikishan@DESKTOP-EQJ818K>
2020-08-19 23:54:21 +0530

    testtt

--- ActiTimeM/src/test/java/com/actiTime/LoginTests/ValidLoginTest.java
+++ ActiTimeM/src/test/java/com/actiTime/LoginTests/ValidLoginTest.java
@@ -1,5 +1,14 @@
 package com.actiTime.LoginTests;
 
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
 public class ValidLoginTest {
+	
+	@Test
+	void run() {
+		
+		Reporter.log("Run Run", true);
+	}
 
 }
